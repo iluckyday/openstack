c704c3559d6efcedbab7a3fdc8e1a9a8d2ea2e3d
iluckyday <luckyday1990@mail.com>
2021-08-27 15:04:25 +0800

    test

--- build-kolla-images.sh
+++ build-kolla-images.sh
@@ -6,15 +6,25 @@ pip install kolla
 LATEST_RELEASE=$(curl -sSkL http://www.openstack.com |  grep -oP 'LATEST RELEASE: \K(.*)(?=<)')
 LATEST_RELEASE=${LATEST_RELEASE,,}
 
-kolla-build -b ubuntu -t binary --openstack-release ${LATEST_RELEASE} barbican cinder designate glance haproxy heat horizon ironic iscsid keepalived keystone kolla-toolbox manila mariadb masakari memcached mistral multipathd ^neutron ^nova octavia openvswitch placement prometheus qdrouterd qdrouterd redis sahara senlin swift tacker tgtd vitrage zookeeper
+DISTRO=ubuntu
+TYPE=binary
+
+kolla-build -b ${DISTRO} -t ${TYPE} --tag ${LATEST_RELEASE} --use-dumb-init --summary --nokeep --openstack-branch ${LATEST_RELEASE} --openstack-release ${LATEST_RELEASE} barbican cinder designate glance haproxy heat horizon ironic iscsid keepalived keystone kolla-toolbox manila mariadb masakari memcached mistral multipathd ^neutron ^nova octavia openvswitch placement prometheus qdrouterd qdrouterd redis sahara senlin swift tacker tgtd vitrage zookeeper
 
 sleep 1
 
-docker image list "kolla/ubuntu-binary-*"
-docker save $(docker image list "kolla/ubuntu-binary-*" -q) | xz > /tmp/kolla-ubuntu-binary-images-${LATEST_RELEASE}.tar.xz
+docker image list "kolla/${DISTRO}-${TYPE}-*"
+docker save $(docker image list "kolla/${DISTRO}-${TYPE}-*" -q) | xz > /tmp/kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
 
-echo kolla-ubuntu-binary-images-${LATEST_RELEASE} is:
-ls -lh /tmp/kolla-ubuntu-binary-images-${LATEST_RELEASE}.tar.xz
+echo kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE} is:
+ls -lh /tmp/kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
+
+for (( n=1; n<=3; n++)); do
+  ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+  [ ! "$ver" ] || break
+done
+
+curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
 for f in /tmp/kolla-*xz; do
 FILENAME=$(basename $f)
--- get-kolla-images.sh
+++ get-kolla-images.sh
@@ -12,14 +12,25 @@ LATEST_RELEASE=${LATEST_RELEASE,,}
 cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc
 cat etc_kolla_globals.yml | sed "s/LATEST_RELEASE/${LATEST_RELEASE}/" | tee /etc/kolla/globals.yml
 
+DISTRO=$(awk -F'"' '/kolla_base_distro/ {print $2}' /etc/kolla/globals.yml)
+TYPE=$(awk -F'"' '/kolla_install_type/ {print $2}' /etc/kolla/globals.yml)
+
 kolla-ansible pull
 
 sleep 1
 
-docker save $(docker image list "kolla/ubuntu-binary-*" -q) | xz > /tmp/kolla-ubuntu-binary-images-${LATEST_RELEASE}.tar.xz
+docker image list "kolla/${DISTRO}-${TYPE}-*"
+docker save $(docker image list "kolla/${DISTRO}-${TYPE}-*" -q) | xz > /tmp/kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
 
-echo kolla-ubuntu-binary-images-${LATEST_RELEASE} is:
-ls -lh /tmp/kolla-ubuntu-binary-images-${LATEST_RELEASE}.tar.xz
+echo kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE} is:
+ls -lh /tmp/kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
+
+for (( n=1; n<=3; n++)); do
+  ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+  [ ! "$ver" ] || break
+done
+
+curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
 for f in /tmp/kolla-*xz; do
 FILENAME=$(basename $f)
