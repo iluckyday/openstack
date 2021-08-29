dacbae1e67e24a05a05e2aadce7b88f648a265a2
iluckyday <luckyday1990@mail.com>
2021-08-29 22:19:27 +0800

    test

--- get-kolla-images.sh
+++ get-kolla-images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
 
+export XZ_DEFAULTS="-9 -T 0"
+
 apt update
 DEBIAN_FRONTEND=noninteractive apt -y install ansible
 
@@ -10,10 +12,14 @@ LATEST_RELEASE=$(curl -sSkL http://www.openstack.com |  grep -oP 'LATEST RELEASE
 LATEST_RELEASE=${LATEST_RELEASE,,}
 
 cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc
-cat etc_kolla_globals.yml | sed "s/LATEST_RELEASE/${LATEST_RELEASE}/" | tee /etc/kolla/globals.yml
 
-DISTRO=$(awk -F'"' '/kolla_base_distro/ {print $2}' /etc/kolla/globals.yml)
-TYPE=$(awk -F'"' '/kolla_install_type/ {print $2}' /etc/kolla/globals.yml)
+DISTRO=ubuntu
+TYPE=source
+
+cat etc_kolla_globals.yml | sed -e "s/DISTRO/$DISTRO/" -e "s/TYPE/$TYPE/" -e "s/LATEST_RELEASE/${LATEST_RELEASE}/" | tee /etc/kolla/globals.yml
+
+#DISTRO=$(awk -F'"' '/kolla_base_distro/ {print $2}' /etc/kolla/globals.yml)
+#TYPE=$(awk -F'"' '/kolla_install_type/ {print $2}' /etc/kolla/globals.yml)
 
 kolla-ansible pull
 
@@ -21,12 +27,8 @@ sleep 1
 
 docker image list "kolla/${DISTRO}-${TYPE}-*"
 
-sleep 1
-
-docker save $(docker image list "kolla/${DISTRO}-${TYPE}-*" -q) | xz > /tmp/dockerhub-kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
-
-echo dockerhub-kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE} is:
-ls -lh /tmp/dockerhub-kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
+DDATE=$(date +%Y%m%d%H%M%S)
+docker save $(docker image list "kolla/${DISTRO}-${TYPE}-*" -q) | xz > /tmp/dockerhub-kolla-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}-${DDATE}.tar.xz
 
 for (( n=1; n<=3; n++)); do
   ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
@@ -35,7 +37,7 @@ done
 
 curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
-for f in /tmp/dockerhub-kolla-*xz; do
+for f in /tmp/dockerhub-kolla-*.tar.xz; do
 FILENAME=$(basename $f)
 SIZE=$(du -h $f | awk '{print $1}')
 trans_url=$(/tmp/transfer wet --silent $f)
