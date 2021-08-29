837fd13e08de14015a30e117ea70be71b9cb1575
iluckyday <luckyday1990@mail.com>
2021-08-29 10:53:02 +0800

    test

--- get-kolla-core-images.sh
+++ get-kolla-core-images.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 set -ex
 
+export XZ_DEFAULTS="-9 -T 0"
+
 apt update
 DEBIAN_FRONTEND=noninteractive apt -y install ansible
 
 pip install kolla-ansible docker
 
+DDATE=$(date +%Y%m%d%H%M%S)
 LATEST_RELEASE=$(curl -sSkL http://www.openstack.com |  grep -oP 'LATEST RELEASE: \K(.*)(?=<)')
 LATEST_RELEASE=${LATEST_RELEASE,,}
 
 cp -r /usr/local/share/kolla-ansible/etc_examples/kolla /etc
-cat etc_kolla_globals.yml | sed "s/LATEST_RELEASE/${LATEST_RELEASE}/" | sed '/openstack_core/,$d' | tee /etc/kolla/globals.yml
 
-DISTRO=$(awk -F'"' '/kolla_base_distro/ {print $2}' /etc/kolla/globals.yml)
-TYPE=$(awk -F'"' '/kolla_install_type/ {print $2}' /etc/kolla/globals.yml)
+#DISTRO=$(awk -F'"' '/kolla_base_distro/ {print $2}' /etc/kolla/globals.yml)
+#TYPE=$(awk -F'"' '/kolla_install_type/ {print $2}' /etc/kolla/globals.yml)
 
-kolla-ansible pull
+for d in ubuntu
+do
+	for t in source binary
+	do
+		cat etc_kolla_globals.yml | sed -e "s/DISTRO/$d/" -e "s/TYPE/$t/" -e "s/LATEST_RELEASE/${LATEST_RELEASE}/" -e '/openstack_core/,$d' | tee /etc/kolla/globals.yml
+		kolla-ansible pull
+		sleep 1
+	done
+done
+
+docker image list "kolla/*"
 
 sleep 1
 
-docker image list "kolla/${DISTRO}-${TYPE}-*"
-
-sleep 1
-
-docker save $(docker image list "kolla/${DISTRO}-${TYPE}-*" -q) | xz > /tmp/dockerhub-kolla-core-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
-
-echo dockerhub-kolla-core-${DISTRO}-${TYPE}-images-${LATEST_RELEASE} is:
-ls -lh /tmp/dockerhub-kolla-core-${DISTRO}-${TYPE}-images-${LATEST_RELEASE}.tar.xz
+for d in ubuntu
+do
+	for t in source binary
+	do
+		docker save $(docker image list "kolla/$d-$t-*" -q) | xz > /tmp/dockerhub-kolla-core-$d-$t-images-${LATEST_RELEASE}-${DDATE}.tar.xz
+	done
+done
 
 for (( n=1; n<=3; n++)); do
   ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
