e247dbb93e5f31cbc3e544afe21bdb4688e51547
iluckyday <luckyday1990@mail.com>
2022-03-07 12:54:25 +0800

    test

---
+++ boot2vmm/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -ex
+
+apt-get update
+apt-get install -y libelf-dev
+
+export KCONFIG_ALLCONFIG=$(pwd)/boot2vmm/build.config
+
+RELEASE=$(curl -skL https://buildroot.org/downloads/Vagrantfile | awk  -F"'" '/RELEASE=/ {print $2}')
+curl -skL https://buildroot.org/downloads/buildroot-${RELEASE}.tar.gz | tar -xz
+cd buildroot-${RELEASE}
+
+make -s allnoconfig
+make -s
+
+cp output/images/rootfs.iso9660 /tmp/boot2vmm.iso
