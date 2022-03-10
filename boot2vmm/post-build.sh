35e3a43cfadf63249387f99f892c054c4e191c14
iluckyday <luckyday1990@mail.com>
2022-03-10 12:44:09 +0800

    test

--- boot2vmm/post-build.sh
+++ boot2vmm/post-build.sh
@@ -3,3 +3,5 @@
 mkdir -p $TARGET_DIR/var/lib/libvirt/qemu $TARGET_DIR/var/lib/libvirt/secrets $TARGET_DIR/var/lib/libvirt/storage
 
 ln -sf /run $TARGET_DIR/var/run
+
+chroot $TARGET_DIR systemctl mask libvirtd.service libvirtd.socket libvirtd-ro.socket libvirtd-admin.socket libvirtd-tcp.socket libvirtd-tls.socket
